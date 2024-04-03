ef24d205f2c15b94d4b0746162a504642d3946ec
ergildo <ergildo@gmail.com>
2024-04-03 16:24:10 -0300

    fix bucket uri

--- .github/actions/deploy-aws-s3/main.js
+++ .github/actions/deploy-aws-s3/main.js
@@ -5,7 +5,7 @@ function run(){
     const region = core.getInput('region', {required: true})
     const dist = core.getInput('dist-folder', {required: true})
 
-    exec.exec(`aws s3 sync ${dist}  s3:\\${backet} --region=${region}`);
+    exec.exec(`aws s3 sync ${dist}  s3://${backet} --region=${region}`);
 
     siteUrl = `http://${backet}.s3-website-${region}.amazonaws.com/`
 
